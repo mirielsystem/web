2e4d69d9693de948fea27a599f29e50a9c46fe94
ありす <master@16439s.jp>
2024-02-25 15:38:34 +0900

    Update raw.php

--- raw.php
+++ raw.php
@@ -12,54 +12,26 @@
         background-color: #f0f0f0;
     }
 
-    h1, h2, h3, h4, p {
+    dl {
         text-align: center;
-        margin: 10px 0;
     }
 
-    a {
-        text-decoration: none;
-        color: #007bff;
+    dt {
+        font-weight: bold;
+        margin-top: 20px;
     }
 
-    a:hover {
-        text-decoration: underline;
+    dd {
+        margin-bottom: 20px;
     }
 
-    hr {
-        border: 1px solid #ddd;
-        margin: 20px 0;
-    }
-
-    .card-container {
-        display: flex;
-        justify-content: space-around;
-        flex-wrap: wrap;
-    }
-
-    .card {
-        width: 250px;
-        margin: 10px;
-        background-color: #fff;
-        border: 1px solid #ddd;
-        padding: 15px;
-        border-radius: 8px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        /* カードの下部に余白を追加 */
-        margin-bottom: 60px;
-    }
-
-    .card img {
-        max-width: 100%;
-        height: auto;
-        border-radius: 4px;
+    p {
+        text-align: center;
+        margin-top: 20px;
     }
 </style>
 </head>
 <body>
-<?php include('header.php'); ?>
-
-<h1>特定商取引法に基づく表記</h1>
 
 <dl>
   <dt>事業者の名称</dt>
@@ -94,6 +66,5 @@
 
 <p>※所在地については、特定事業者に関する情報公表制度に基づき、請求があった場合に開示します。</p>
 
-<?php include('footer.php'); ?>
 </body>
 </html>
