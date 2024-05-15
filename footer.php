7872dc38e35f60085fad73b0594cb2997cc3614d
Miriel <me@miriel.net>
2024-05-15 18:41:43 +0900

    Fix: P2

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 </head>
 <body>
     <div id="footer" style="position: fixed; bottom: 0; width: 100%; background-color: #333; color: #fff; padding: 10px 20px; text-align: center;">
-        <p>Copyright © 2024 FreelyNetwork All Rights Reserved.</p>
+        <p>Copyright © 2024 Miriel Systems All Rights Reserved.</p>
     </div>
 </body>
 </html>
